071f5519476991adaf0b373b0c30c6a1723f1297
mintchoco5555 <rossarin.m@kkumail.com>
2025-08-15 22:01:06 +0700

    ออกแบบหน้าปกเกม

---
+++ demo/Scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_bn_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_bn_setting_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_bn_exit_pressed() -> void:
+	get_tree().quit()
